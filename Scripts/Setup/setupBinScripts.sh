cb5d152fd2e22e657f53cbc675651c52519cac76
roflcopter4 <brendan.leason.4@gmail.com>
2018-02-28 22:13:45 -0700

    update

--- Scripts/Setup/setupBinScripts.sh
+++ Scripts/Setup/setupBinScripts.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir -p "$HOME/.local/bin"
-"$HOME/personaldotfiles/Scripts/misc/ln_sh_scripts.sh" "$HOME/personaldotfiles/Scripts/homebin" "$HOME/.local/bin"
+"$HOME/personaldotfiles/Scripts/misc/ln_sh_scripts.sh" "$HOME/personaldotfiles/Scripts/bin" "$HOME/.local/bin"
 
 # Neovim-QT
-if [ "$(uname)" = 'Cygwin' -o "$(uname)" = 'Msys' ]; then
+if [ "$(uname)" = 'Cygwin' ] || [ "$(uname)" = 'Msys' ]; then
     ln -sf "$HOME/personaldotfiles/Scripts/viRelated/run-win_nvim_qt.sh" "$HOME/.local/bin/vi"
 else
     ln -sf "$HOME/personaldotfiles/Scripts/viRelated/run_vi.sh" "$HOME/.local/bin/gvi"
